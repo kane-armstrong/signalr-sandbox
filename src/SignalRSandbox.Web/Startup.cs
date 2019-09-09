a98839f0b731e75a1ef9b5510db6e9a0684f8790
Kane Armstrong <kane.armstrong@outlook.com>
2019-09-09 12:05:01 +1200

    Clients now get notifications regardless of node

--- src/SignalRSandbox.Web/Startup.cs
+++ src/SignalRSandbox.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace SignalRSandbox.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            services.AddSignalR().AddRedis("host.docker.internal:6379");
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
